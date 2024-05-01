6bca02735defec82b1c4af038fce1cd9984c6ec3
João Victor Ribeiro Santos <joaovictoribeirosantos11@gmail.com>
2024-05-01 12:23:04 -0300

    fix avaliação

--- roleta.c
+++ roleta.c
@@ -57,20 +57,21 @@ int main(int argc, char const *argv[])
 
 //Função de avaliação
 void avaliar(cromossomo populacao[]) {
-
+    int j;
 
     for (size_t i = 0; i < POPULACAO; i++)
     {
         double distanciaTotal = 0;
 
-        for (size_t j = 0; j < N_CIDADES -1; j++)
+        for ( j = 0; j < N_CIDADES -1; j++)
         {
             distanciaTotal += distancia(populacao[i].cidades[j], populacao[i].cidades[j + 1]);
         }
+
+        distanciaTotal += distancia(populacao[i].cidades[0], populacao[i].cidades[j + 1]);//Distancia da ultima cidade até a inicial
         populacao[i].custo = distanciaTotal;
-
     }
-
+    
 }
 
 //Calcula distancia entre duas cidades
